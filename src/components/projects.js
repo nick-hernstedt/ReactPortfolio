21fa0095d6e70f859289bf5bc8aa97c08e2ae5e7
copiousamounts <nick.hernstedt@gmail.com>
2020-05-18 15:50:02 -0700

    did some styling and formatting for projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,119 @@
 import React, { Component } from "react";
+import {
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText,
+} from "react-mdl";
 
 class Projects extends Component {
   render() {
     return (
       <div>
-        <h1>Projects Page</h1>
+        <div className="projects-grid">
+          <Card shadow={0} style={{ width: "512px", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover",
+              }}
+            >
+              Welcome
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={0} style={{ width: "512px", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover",
+              }}
+            >
+              Welcome
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
+        <div className="projects-grid">
+          <Card shadow={0} style={{ width: "512px", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover",
+              }}
+            >
+              Welcome
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={0} style={{ width: "512px", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover",
+              }}
+            >
+              Welcome
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       </div>
     );
   }
