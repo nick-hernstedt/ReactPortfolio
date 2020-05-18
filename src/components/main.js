7d42ea71bfa8327b92f54a0b2e8923bca2d60b7e
copiousamounts <nick.hernstedt@gmail.com>
2020-05-18 13:28:05 -0700

    set up routes in main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import LandingPage from "./landingpage";
 import About from "./aboutme";
+import Resume from "./resume";
+import Projects from "./projects";
+import Contact from "./contact";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route exact path="/" component={LandingPage} />
-    <Route exact path="/" component={LandingPage} />
-    <Route exact path="/" component={LandingPage} />
-    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={About} />
+    <Route path="/resume" component={Resume} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/contact" component={Contact} />
   </Switch>
 );
 
