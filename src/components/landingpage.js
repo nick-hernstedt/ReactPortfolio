d689af667c17f5154b8c53b4ed4f16b073111bca
copiousamounts <nick.hernstedt@gmail.com>
2020-05-18 14:25:38 -0700

    did some styling on landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,10 @@ class Landing extends Component {
       <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img src="" />
+            <img src="myAvatar.png" alt="avatar" className="avatar-img" />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+            </div>
           </Cell>
         </Grid>
       </div>
