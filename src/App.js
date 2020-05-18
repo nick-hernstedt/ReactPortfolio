28e5b5d2122a4f7e8b41d3587fc42bd53f5e15d7
copiousamounts <nick.hernstedt@gmail.com>
2020-05-18 13:54:46 -0700

    links now route to correct pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
 import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -9,18 +10,18 @@ function App() {
       <Layout>
         <Header title="Title" scroll>
           <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
         <Drawer title="Title">
           <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
           </Navigation>
         </Drawer>
         <Content>
